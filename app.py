b1543543294c4682a58c6211bac8ab3eab50fd61
Nemanja Bozic <A00222186@student.ait.ie>
2019-03-06 11:39:49 +0000

    Changed from DB Test to DB Usertable. File outputs hello + username from db.

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 from flaskext.mysql import MySQL
 
 
 app = Flask(__name__)
-mysql=MySQL()
+mysql = MySQL()
 
 #Mysql configurations
 app.config['MYSQL_DATABASE_USER']='root'
-app.config['MYSQL_DATABASE_PASSWORD']='password'
-app.config['MYSQL_DATABASE_DB']='test'
+app.config['MYSQL_DATABASE_PASSWORD']='admin'
+app.config['MYSQL_DATABASE_DB']='yuanshop'
 app.config['MYSQL_DATABASE_HOST']='localhost'
 mysql.init_app(app)
 
@@ -16,13 +16,13 @@ mysql.init_app(app)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/user/<name>')
-def user_route(name):
+@app.route('/user/<username>')
+def user_route(username):
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor.execute("SELECT * from User")
-    data=cursor.fetchone()
-    return render_template('index.html',name=name);
+    cursor.execute("SELECT * from USERTABLE")
+    data = cursor.fetchone()
+    return render_template('index.html',username=username);
 
 if __name__ == '__main__':
     app.run()
