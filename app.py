1f9d69bc52bc872681d6d1399f3fcf87f00eaa94
AshleyXiLan <A00248567@student.ait.ie>
2019-03-26 13:05:07 +0000

    with productdetail

--- app.py
+++ app.py
@@ -1,40 +1,52 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request
 from flaskext.mysql import MySQL
 
-
 app = Flask(__name__)
-mysql=MySQL()
+mysql = MySQL()
 
 #Mysql configurations
 app.config['MYSQL_DATABASE_USER']='root'
 app.config['MYSQL_DATABASE_PASSWORD']='admin'
 app.config['MYSQL_DATABASE_DB']='yuanshop'
 app.config['MYSQL_DATABASE_HOST']='localhost'
-app.config['MYSQL_DATABASE_PORT']=3307
+app.config['MYSQL_DATABASE_CHARSET']='utf8'
+app.config['MYSQL_USE_UNICODE']= True
 mysql.init_app(app)
+conn = mysql.connect()
+cursor = conn.cursor()
+
+
+
+def findID(id):
+    id = request.values.get(id)
+    return id
+
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
 
-@app.route('/user/<name>')
-def user_route(name):
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * from User")
-    data=cursor.fetchone()
-    return render_template('index.html',name=name)
+    return render_template('index.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'secret':
-            error = 'Invalid Username/Password.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+@app.route('/product/<id>')
+def getPro(id):
+    ID = findID(id)
+    cursor.execute('''SELECT * from Product where productid like %s''' % id)
+    productid = cursor.fetchone()
+
+
+
+    print productid[0]
+    print productid[1]
+    print productid[2]
+    print productid[3]
+    print productid[4]
+    print productid[5]
+    print productid[6]
+    print productid[7]
+
+    return render_template('product-details.html', ID=ID, productid=productid)
 
 
 
