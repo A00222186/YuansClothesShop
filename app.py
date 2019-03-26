96dab159e3bd84252b280c5c93f7d05b55f6ce75
AshleyXiLan <A00248567@student.ait.ie>
2019-03-26 12:29:08 +0000

    productdetail

--- app.py
+++ app.py
@@ -9,26 +9,12 @@ app.config['MYSQL_DATABASE_USER']='root'
 app.config['MYSQL_DATABASE_PASSWORD']='admin'
 app.config['MYSQL_DATABASE_DB']='yuanshop'
 app.config['MYSQL_DATABASE_HOST']='localhost'
-app.config.setdefault('MYSQL_CHARSET','utf8')
+app.config['MYSQL_DATABASE_CHARSET']='utf8'
 app.config['MYSQL_USE_UNICODE']= True
 mysql.init_app(app)
 conn = mysql.connect()
 cursor = conn.cursor()
 
-
-class product():
-
-    def __init__(self,id,gender,age,producttype,productsize,price,stock):
-        self.id = id
-        self.gender = gender
-        self.age = age
-        self.producttype = producttype
-        self.productsize = productsize
-        self.price = price
-        self.stock = stock
-
-    def __repr__(self):
-        return "%s" % self.producttype
 
 
 def findID(id):
@@ -36,44 +22,31 @@ def findID(id):
     return id
 
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+
+    return render_template('index.html')
 
 
 @app.route('/product/<id>')
-
-
-
 def getPro(id):
     ID = findID(id)
     cursor.execute('''SELECT * from Product where productid like %s''' % id)
     productid = cursor.fetchone()
-    cursor.execute('''SELECT name from Product where productid LIKE %s''' % id)
-    name = cursor.fetchone()
-    cursor.execute('''SELECT color from Product where productid LIKE %s''' % id)
-    color = cursor.fetchone()
-    cursor.execute('''SELECT overview from Product where productid LIKE %s''' % id)
-    overview=cursor.fetchone()
-    cursor.execute('''SELECT productsize from Product where productid LIKE %s''' % id)
-    productsize = cursor.fetchone()
-    cursor.execute('''SELECT price from Product where productid LIKE %s''' % id)
-    price = cursor.fetchone()
-    cursor.execute('''SELECT stock from Product where productid LIKE %s''' % id)
-    stock = cursor.fetchone()
-    cursor.execute('''SELECT description from Product where productid LIKE %s''' % id)
-    description = cursor.fetchone()
 
-    print productid
-    print name
-    print color
-    print overview
-    print productsize
-    print price
-    print stock
-    print description
-    return render_template('product-details.html', ID=ID, productid=productid,name=name,color=color,overview=overview,productsize=productsize,
-                           price=price,stock=stock,description=description)
+
+
+    print productid[0]
+    print productid[1]
+    print productid[2]
+    print productid[3]
+    print productid[4]
+    print productid[5]
+    print productid[6]
+    print productid[7]
+
+    return render_template('product-details.html', ID=ID, productid=productid)
 
 
 
