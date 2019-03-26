bc3290503ff98c2886e3412791916f72ac2751f1
Shwetank Anand <shwetanknnd@gmail.com>
2019-03-26 20:11:26 +0530

    final login

--- app.py
+++ app.py
@@ -11,46 +11,41 @@ app.config['MYSQL_DATABASE_USER']='root'
 app.config['MYSQL_DATABASE_PASSWORD']='shwetank'
 app.config['MYSQL_DATABASE_DB']='yuanshop'
 app.config['MYSQL_DATABASE_HOST']='localhost'
-app.config['MYSQL_DATABASE_PORT']=3307
+app.config['MYSQL_DATABASE_PORT']=3306
 mysql.init_app(app)
 
-@app.route('/loginsuccess')
+@app.route('/')
 def index():
-    if 'username' is session:
-        username_session = escape(session['username']).capitalise()
-        return render_template('cart.html',session_user_name=username_session)
-    return redirect(url_for('login.html'))
+    return render_template("index.html", title= 'Login')
 
-@app.route('/user/<name>')
-def user_route(name):
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title="data")
+
+@app.route('/checkUser', methods=['POST'])
+def check():
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor.execute("SELECT * from User")
-    data=cursor.fetchone()
-    return render_template('index.html',name=name)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template("login.html")
-    conn = mysql.connect()
-    error = None
-    if 'username' in session:
-        return redirect(url_for('login'))
-    if request.method == 'POST':
-        username_form = request.form['name']
-        password_form = request.form['password']
-        conn.execute("SELECT COUNT(1) FROM user WHERE username = %s;", [username_form])
-        if conn.fetchall():
-            conn.execute("SELECT password FROM user WHERE username = %s;", [username_form])
-            if password_form == row[0]:
-                session['username'] = request.form["username"]
-                return redirect(url_for('index'))
-            raise SyntaxError('invalid Password')
+    username = str(request.form["name"])
+
+
+    userpassword = str(request.form["password"])
+
+    name = cursor.execute("SELECT username FROM usertable WHERE username = '"+username+"'")
+    print(username)
+
+    #print(username)
+    if not name:
+        print("no user")
+        return render_template("login.html")
     else:
-       error = "Invalid Credential"
-    return render_template('login.html', error=error)
+        print("valid user")
+        return render_template("cart.html")
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
